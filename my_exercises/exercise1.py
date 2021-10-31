2d4828a13ba4acf98a26717bcd11626b919616f3
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-10-31 21:37:51 +0400

    max

--- my_exercises/exercise1.py
+++ my_exercises/exercise1.py
@@ -1,12 +1,12 @@
 import math
 
 
-# def exercise1():
-#     print("Enter integer")
-#     x = input()
-#     y = input()
-#     k = (int(y)+1) * (int(x) + pow((int(x)**2 + 1), 6) * math.sin(int(x)**2-3) - math.tan(int(y)))
-#     print(k)
+def exercise1():
+    print("Enter integer")
+    x = input()
+    y = input()
+    k = (int(y)+1) * (int(x) + pow((int(x)**2 + 1), 6) * math.sin(int(x)**2-3) - math.tan(int(y)))
+    print(k)
 
 def exercise11():
     print("Enter integers")
@@ -21,6 +21,12 @@ def exercise11():
         y = -3
     print(y)
 
+def exercise21(x, y, z):
+    k = max(x,y,z)
+    return k
 
-# exercise1()
+
+exercise1()
 exercise11()
+m = exercise21(5,6,8)
+print(m)
