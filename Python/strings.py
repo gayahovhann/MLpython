ceea66bbbd999d8a4f461830fa206825cdc40836
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-01-29 16:26:13 +0400

    exercises for practice

--- Python/strings.py
+++ Python/strings.py
@@ -8,3 +8,15 @@ def ex646(s):
 print(ex646('hafewgfaaaablanih'))
 
 
+def ex657(s):
+    new_s = ''
+    for i in range(0, len(s)-1, 2):
+        if s[i] > s[i+1]:
+            new_s = new_s + s[i]
+        else:
+            new_s = new_s + s[i+1]
+    if len(s)%2==1:
+        new_s = new_s + s[-1]
+
+    return new_s
+print(ex657('aaaaa'))
