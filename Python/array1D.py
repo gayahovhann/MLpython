9e23576a0115778d0407da30aa9f04755f01d052
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-01-30 12:29:01 +0400

    exercises for practice

--- Python/array1D.py
+++ Python/array1D.py
@@ -8,3 +8,11 @@ def ex211(n_vector):
     return s/k
 print(ex211([1, 2, 3, 5]))
 
+def ex241(n_vec, k):
+    s = 0
+    for i in range(len(n_vec)):
+        if n_vec[i] % 3 == 0:
+            s = s + n_vec[i]
+    return s
+print(ex241([2, 6, 3, 6], 3))            
+
