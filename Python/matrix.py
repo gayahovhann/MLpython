6fc8ab5c1a79140dbb898d146b54c15a7458f7af
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-01-31 13:19:45 +0400

    exercises for practice

--- Python/matrix.py
+++ Python/matrix.py
@@ -6,3 +6,11 @@ def ex421(matrix_m, k):
             if matrix_m[i][j] % k == 0:
                 a +=1
     return a
+
+def ex446(m_matrix, a, b):
+    s=0
+    for i in range(0, len(m_matrix)):
+        for j in range(i, len(m_matrix)):
+            if m_matrix[i][j] >= a and m_matrix[i][j]<= b:
+                s+=m_matrix[i][j]
+    return s
