3f72245aec608b2bab63b105b12ab45f0adacfa5
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-02-14 20:30:20 +0400

    exercises for practice

--- Python/tuple_set_dict.py
+++ Python/tuple_set_dict.py
@@ -99,3 +99,15 @@ def ex_dict9():
     }
     list_of_keys = sample_dict.values()
     return min(list_of_keys)
+
+def ex_dict10():
+    # Write a Python program to change Brad’s salary 
+    # to 8500 in the following dictionary.
+
+    sample_dict = {
+        'emp1': {'name': 'Jhon', 'salary': 7500},
+        'emp2': {'name': 'Emma', 'salary': 8000},
+        'emp3': {'name': 'Brad', 'salary': 500}
+    }
+    sample_dict['emp3']['salary'] = 8500
+    print(sample_dict)
