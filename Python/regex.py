54d455e85d508c02331e5a1fae8fe0b07bd7638e
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-03-01 15:26:30 +0400

    exercise for regex

--- Python/regex.py
+++ Python/regex.py
@@ -1,7 +1,12 @@
-example1
+#example1
 import re
 s1 = 'All I want for Christmas is you, baby'
 
 match1 = re.search(r'is', s1)
 print(match1.start())
 print(match1.end())
+
+#example2
+s2 = 'Take me. to church'
+match = re.search(r'\.', s2)
+print(match)
