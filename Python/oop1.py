ea7ff33651617c57837f63ec1238634fafca064f
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-03-12 16:18:47 +0400

    inheritance

--- Python/oop1.py
+++ Python/oop1.py
@@ -6,3 +6,10 @@ class Animals:
     def aboutanimal(self):
         print("I am animal, my weight is{} kg, my height is {} cm".format(self.weight, self.height))
 
+class Graund(Animals):
+    def __init__(self, height, weight, velocity_on_ground):
+        super().__init__(height, weight)
+        self.velocity_on_ground = velocity_on_ground
+
+    def aboutground(self):
+        print("I am animal, my weight and height are {} kg and {} cm, my velocity on graound is {}".format(self.weight, self.height, self.velocity_on_ground))
