8fa98c5d3733e3afd518e2939ef57b084f5c4216
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-03-12 16:13:38 +0400

    inheritance

---
+++ Python/oop1.py
@@ -0,0 +1,8 @@
+class Animals: 
+    def __init__(self, height, weight):
+        self.height = height
+        self.weight = weight
+
+    def aboutanimal(self):
+        print("I am animal, my weight is{} kg, my height is {} cm".format(self.weight, self.height))
+
