3899cf4dcdb7dea7b65a3160a006bc3739510d76
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-03-23 23:04:53 +0400

    working with magic methods

--- Python/magicmethods.py
+++ Python/magicmethods.py
@@ -9,3 +9,18 @@ class MathFunc:
         self.frac1 = fractions.Fraction(self.n1, self.n2)
         self.frac2 = fractions.Fraction(self.n3, self.n4)
 
+    def __add__(self):
+        return self.frac1 + self.frac2
+
+    def __sub__(self):
+        return self.frac1 - self.frac2
+
+    def __mul__(self):
+        return self.frac1 * self.frac2
+
+    def __divmod__(self):
+        return self.frac1 / self.frac2
+
+
+vle = MathFunc(1, 2, 1, 3)
+print(vle.__divmod__())
