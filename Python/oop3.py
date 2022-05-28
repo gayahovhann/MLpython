f17c8057da22910b40e79c0ee03ca3ccddb0af05
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-05-28 22:44:34 +0400

    OOP

---
+++ Python/oop3.py
@@ -0,0 +1,25 @@
+class Mother():
+    def __init__(self):
+        pass
+
+    def eyes(self):
+        print("My mother's eyes are green")
+
+    def nose(self):
+        print("My mother's nose is beautiful")
+
+
+class Father():
+    def __init__(self):
+        pass
+
+    def eyes(self):
+        print("My father's eyes are brown")
+
+    def nose(self):
+        print("My father's nose is big")
+
+moth = Mother()
+moth.eyes()
+moth = Father()
+moth.eyes()
